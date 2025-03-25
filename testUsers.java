cdc6a63d0b9d2eac5ad4d6da80d8ab3eee89a4ef
Aidan DiNunzio <148111473+DiNunzio-Aidan11@users.noreply.github.com>
2025-03-24 21:48:35 -0700

    Update testUsers.java

--- testUsers.java
+++ testUsers.java
@@ -5,26 +5,29 @@ import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
 
+import java.io.File;
+
 import org.junit.Test;
 
 import model.Users;
 
 public class TestUsers {
+	File testFile = new File("testing.txt");
 	
 	@Test
 	public void testAdding() {
 		Users users = new Users();
-		users.addUser("Joe", "123");
+		users.addUser("Joe", "123", testFile);
 		assertTrue(users.userExists("Joe"));
 		
 		// testing duplicate username
-		assertFalse(users.addUser("Joe", "password"));
+		assertFalse(users.addUser("Joe", "password", testFile));
 	}
 	
 	@Test
 	public void testGetUser() {
 		Users users = new Users();
-		users.addUser("Joe", "123");
+		users.addUser("Joe", "123", testFile);
 		assertNotNull(users.getUser("Joe"));
 		
 		// non existing user
@@ -32,20 +35,11 @@ public class TestUsers {
 	}
 	
 	@Test
-	public void testRemoveUser() {
-		Users users = new Users();
-		users.addUser("Joe", "123");
-		
-		users.removeUser("Joe");
-		assertNull(users.getUser("Joe"));
-	}
-	
-	@Test
 	public void testUserExists() {
 		Users users = new Users();
 		assertFalse(users.userExists("Joe"));
 		
-		users.addUser("Joe", "123");
+		users.addUser("Joe", "123", testFile);
 		assertTrue(users.userExists("Joe"));
 		
 	}
@@ -53,7 +47,7 @@ public class TestUsers {
 	@Test
 	public void testAuthentication() {
 		Users users = new Users();
-		users.addUser("Joe", "123");
+		users.addUser("Joe", "123", testFile);
 		assertFalse(users.authenticate("Joe", "wrongPassword"));
 		assertTrue(users.authenticate("Joe", "123"));
 		
@@ -62,5 +56,4 @@ public class TestUsers {
 	}
 
 }
-
 
